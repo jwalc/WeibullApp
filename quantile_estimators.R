87c22e39381b4c547b7924506b6c8194086f0724
Jan Walczak <walczak.jan@web.de>
2020-08-14 18:17:25 +0200

    Adds functionality to johnson_method

--- quantile_estimators.R
+++ quantile_estimators.R
@@ -165,6 +165,38 @@ kaplan_meier_method <- function (in_data, time = "time", event = "event", n_even
   return(df)
 }
 
-johnson_method <- function (in_data, time = "time", event = "event") {
+johnson_method <- function (in_data, time = "time", event = "event", n_events = "n_events") {
   
+  time_ <- as.symbol(time)
+  event_ <- as.symbol(event)
+  n_events_ <- as.symbol(n_events)
+  
+  if (!n_events %in% names(in_data)) {
+    warning("Invalid 'n_events' argument! Assuming one event per row!")
+    in_data["n_events"] <- rep(1, dim(in_data)[1])
+  }
+  
+  N <- base::sum(in_data[n_events])
+  
+  df <- in_data %>%
+    dplyr::arrange(!!time_) %>%
+    dplyr::mutate(n_out = base::cumsum(!!n_events_) - !!n_events_) %>%
+    dplyr::group_by(!!time_) %>%
+    dplyr::mutate(n_out = base::min(n_out)) %>%
+    dplyr::ungroup() %>%
+    dplyr::filter(!!event_ == 0)
+  
+  k <- base::dim(df)[1]
+  df["j"] <- c(as.numeric((N + 1) / (N + 1 - df["n_out"][1,])), base::rep(0, k-1))
+  for (i in 2:k) {
+    delta_j <- ((N + 1 - df["j"][i-1,]) / (N + 1 - df["n_out"][i,])) * df["n_events"][i,]
+    df["j"][i,] <- df["j"][i-1,] + delta_j
+  }
+  
+  df <- df %>%
+    dplyr::mutate(F_i = (j - 0.3) / (N + 0.4))
+  
+  df <- dplyr::full_join(in_data, df[c(time, event, "F_i")], by = c(time, event))
+  
+  return(df)
 }
