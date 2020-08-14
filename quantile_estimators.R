ed84a9d9fe47efac832632d4a01f680a3af55768
Jan Walczak <walczak.jan@web.de>
2020-08-14 13:33:45 +0200

    Adds median rank regression function

--- quantile_estimators.R
+++ quantile_estimators.R
@@ -1,3 +1,33 @@
 input_handler <- function (in_data) {
   return(in_data)
 }
+
+mr_regression <- function (in_data, time = "time", event = "event", simplified = FALSE) {
+  
+  time_ <- base::as.symbol(time)
+  event_ <- base::as.symbol(event)
+  
+  df <- in_data %>%
+    dplyr::arrange(!!time_) %>%
+    dplyr::filter(!!event_ == 0) %>%
+    dplyr::mutate(rank = seq(1:n()))
+  
+  if (simplified) {
+    df <- df %>%
+      dplyr::mutate(F_i = rank / (n() + 1))
+  } else if (!simplified) {
+    df <- df %>%
+      dplyr::mutate(F_i = (rank - 0.3) / (n() + 0.4))
+  } else {
+    warning("Invalid input for parameter 'simplified'!")
+    return(in_data)
+  }
+  
+  if (!all(in_data[event] == 0)) {
+    warning("mr_regression does not consider right censored data!")
+    df <- dplyr::full_join(in_data, df[c(time, event, "rank", "F_i")], by = c(time, event)) %>%
+      dplyr::arrange(rank)
+  }
+  
+  return(df)
+}
