fd5649019068ebe083e0edd40a245ed86382345c
Jan Walczak <walczak.jan@web.de>
2020-08-15 14:57:04 +0200

    Adds dummy function for computing Weibull x-axis limits

--- plotting_functions.R
+++ plotting_functions.R
@@ -6,6 +6,10 @@ weibull_y_axis_ <- list(scale = scales::trans_new(name = "weibull_y",
                         labels = c(0.001, 0.002, 0.003, 0.005, 0.01, 0.02, 0.03, 0.05, seq(0.1, 0.9, by = 0.1), 0.95, 0.99, 0.999) * 100
 )
 
+weibull_x_limits <- function (x_vals) {
+  
+}
+
 weibull_q_plot <- function (in_data, time = "time", q = "F_i") {
   time_ <- as.symbol(time)
   q_ <- as.symbol(q)
