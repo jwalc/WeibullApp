3991c740a29c780c2c06d5ffeb88cbdde1f9f44b
Unknown <walczak.jan@web.de>
2020-08-23 10:38:24 +0200

    FIX: mr_regression supports data with n_events != 1
    Adds functionality to convert a Kaplan-Meier-style dataframe with multiple events per row.

--- WeibullApp/functions/quantile_estimators.R
+++ WeibullApp/functions/quantile_estimators.R
@@ -32,7 +32,7 @@ input_handler <- function (in_data) {
   }
 }
 
-mr_regression <- function (in_data, time = "time", event = "event", simplified = FALSE, append = FALSE) {
+mr_regression <- function (in_data, time = "time", event = "event", n_events = NA, simplified = FALSE, append = FALSE) {
   #' @title Median Rank Regression for Weibull Quantiles
   #' 
   #' Calculates quantile estimates for the Weibull distribution using median rank regression.
@@ -40,18 +40,23 @@ mr_regression <- function (in_data, time = "time", event = "event", simplified =
   #'  - default: F_i = (rank - 0.3) / (n + 0.4)
   #'  - simplified: F_i = rank / (n + 1)
   #' Coding of events:
-  #'  - 0: time to failure
-  #'  - 1: survival (right censored data)
+  #'  - 1: time to failure
+  #'  - 0: survival (right censored data)
+  #'  
+  #'  @details This method absolutely needs one(!) event per row. Hence you must specify n_events when
+  #'  using method with a Kaplan-Meier-style dataframe. Otherwise the estimator is incorrect!
   #' 
   #' @param in_data numeric vector or tibble containing time to failure
   #' @param time character, name of column containing time data
   #' @param event character, name of column containing event type
+  #' @param n_events character, optional name of column containing number of events for row
   #' @param simplified boolean, specifying use of simplified estimator
   #' @param append boolean, specifying if quantiles should be appended to the given in_data tibble 
   #' @return tibble, returns a tibble with added columns rank and estimated quantiles
   
   time_ <- base::as.symbol(time)
   event_ <- base::as.symbol(event)
+  n_events_ <- base::as.symbol(n_events)
   
   df <- input_handler(in_data)
   if (nrow(df) == 0) {
@@ -61,6 +66,12 @@ mr_regression <- function (in_data, time = "time", event = "event", simplified =
   if (!event %in% names(df)) {
     warning("Assuming all events are failures.")
     df[event] <- base::rep(1,base::dim(df)[1])
+  }
+  
+  if (!is.na(n_events)) {
+    # methods needs one event per row
+    df <- df %>%
+      uncount(!!n_events_)
   }
   
   df <- df %>%
