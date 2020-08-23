b45fdfc56f7e664df318aae15313e0ea92a79fa8
Unknown <walczak.jan@web.de>
2020-08-23 18:29:24 +0200

    Removes append from nelson_method

--- WeibullApp/R/nelson_method.R
+++ WeibullApp/R/nelson_method.R
@@ -8,7 +8,7 @@
 #' @param append boolean, specifying if quantiles should be appended to the given in_data tibble 
 #' @return tibble with added quantile estimations
 
-nelson_method <- function (in_data, time = "time", event = "event", append = FALSE) {
+nelson_method <- function (in_data, time = "time", event = "event") {
 
   time_ <- as.symbol(time)
   event_ <- as.symbol(event)
@@ -20,14 +20,11 @@ nelson_method <- function (in_data, time = "time", event = "event", append = FAL
     dplyr::mutate(lambda_i = 1 / rank) %>%
     dplyr::mutate(H_i = base::cumsum(lambda_i)) %>%
     dplyr::mutate(F_i = 1 - base::exp(- H_i))
-  
-  if (append) {
-    df <- dplyr::full_join(in_data, df[c(time, event, "F_i")], by = c(time, event))
-  } else {
-    df <- df %>%
-      select(c(time, "F_i")) %>%
-      filter(!is.na(F_i))
-  }
+
+  df <- df %>%
+    select(c(time, "F_i")) %>%
+    filter(!is.na(F_i))
+
   
   df["method"] <- "Nelson"
   
