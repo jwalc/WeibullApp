5a24a8cc9da8ca965b21b078be0b4369209ebbd4
Unknown <walczak.jan@web.de>
2020-08-23 19:17:06 +0200

    Adds nelson_method to test script

--- WeibullApp/run_tests.R
+++ WeibullApp/run_tests.R
@@ -2,13 +2,16 @@ library(testthat)
 library(covr)
 
 source("R/mr_regression.R")
+source("R/nelson_method.R")
 source("R/quantile_estimators.R")
 
 testthat::test_dir("tests/testthat/")
 
 coverage <- covr::file_coverage(
-  source_files = c("R/mr_regression.R"),
-  test_files = c("tests/testthat/test_mr_regression.R")
+  source_files = c("R/mr_regression.R",
+                   "R/nelson_method.R"),
+  test_files = c("tests/testthat/test_mr_regression.R",
+                 "tests/testthat/test_nelson_method.R")
 )
 
 report(coverage)
