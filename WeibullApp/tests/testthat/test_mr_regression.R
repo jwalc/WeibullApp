c333f18e15cf5d5c043727440afecab0e1a5c3dd
Unknown <walczak.jan@web.de>
2020-08-23 16:02:46 +0200

    Adds test cases for mr_regression
    Cases for n_events != 1 and event != 1

--- WeibullApp/tests/testthat/test_mr_regression.R
+++ WeibullApp/tests/testthat/test_mr_regression.R
@@ -4,17 +4,27 @@ df_1_in <- tibble::tibble(time = c(100, 200, 300),
 df_1_exp <- tibble::tibble(F_i_simplified = c(0.25, 0.5, 0.75),
                            F_i_default = c(7/34, 0.5, 27/34))
 
+df_2_in <- tibble::tibble(time = c(100, 200, 300),
+                          event = c(1, 0, 1),
+                          n_events = c(1, 2, 2))
+
+df_2_exp <- df_1_exp
+
 # --- Testing correct computation --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 testthat::context("Estimator works")
 
 testthat::test_that("Default Estimator", {
   expect_equal(mr_regression(in_data = df_1_in, time = "time", event = "event", n_events = "n_events", simplified = FALSE)$F_i,
                expected = df_1_exp$F_i_default)
+  expect_equal(mr_regression(in_data = df_2_in, time = "time", event = "event", n_events = "n_events", simplified = FALSE)$F_i,
+               expected = df_2_exp$F_i_default)
 })
 
 testthat::test_that("Simplified Estimator", {
   expect_equal(mr_regression(in_data = df_1_in, time = "time", event = "event", n_events = "n_events", simplified = TRUE)$F_i,
                expected = df_1_exp$F_i_simplified)
+  expect_equal(mr_regression(in_data = df_2_in, time = "time", event = "event", n_events = "n_events", simplified = TRUE)$F_i,
+               expected = df_2_exp$F_i_simplified)
 })
 
 # --- Testing expected output types --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
@@ -53,4 +63,4 @@ testthat::test_that("Invalid simplified", {
 })
 
 
-rm(df_1_in, df_1_exp)
+rm(df_1_in, df_1_exp, df_2_in, df_2_exp)
