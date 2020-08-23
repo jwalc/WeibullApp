b0ff10b23060cc31381bc20a4817acb29f08ab72
Unknown <walczak.jan@web.de>
2020-08-23 18:33:44 +0200

    Adds functionality to nelson_method
    nelson_method can now handle Kaplan-Meier-style dataframes correctly

--- WeibullApp/R/nelson_method.R
+++ WeibullApp/R/nelson_method.R
@@ -2,16 +2,27 @@
 #' 
 #' Computes Weibull quantiles using the Nelson Method. Suitable for right censored data.
 #' 
+#' @details This method needs data with one event per row! If data of a
+#' Kaplan-Meier-style dataframe with multiple events per row is given, you must(!)
+#' pass the n_events column name for correct computation!
+#' 
 #' @param in_data tibble, containing time to failure and event data
 #' @param time character, name of column containing time data
 #' @param event character, name of column containing event type
-#' @param append boolean, specifying if quantiles should be appended to the given in_data tibble 
+#' @param n_events character, optional name for column with number of events per row
 #' @return tibble with added quantile estimations
 
-nelson_method <- function (in_data, time = "time", event = "event") {
+nelson_method <- function (in_data, time = "time", event = "event", n_events = NA) {
 
   time_ <- as.symbol(time)
   event_ <- as.symbol(event)
+  n_events_ <- as.symbol(n_events)
+  
+  if (!is.na(n_events)) {
+    # methods needs one event per row
+    df <- df %>%
+      uncount(!!n_events_)
+  }
   
   df <- in_data %>%
     dplyr::arrange(!!time_) %>%
